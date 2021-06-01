b7917f1a20ff847a078b2d30b6b2fb5de9dc1180
yystatic <yyagi@microsoft.com>
2021-06-01 23:03:18 +0900

    Init app

--- app.org.py
+++ app.org.py
@@ -20,17 +20,18 @@ def index():
     if request.method == 'POST':
         task_content = request.form['content']
         new_task = Todo(content=task_content)
-        
+
         try:
             db.session.add(new_task)
             db.session.commit()
             return redirect('/')
         except:
             return 'There was an issue adding your task'
-            
+
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
+
 
 @app.route('/delete/<int:id>')
 def delete(id):
@@ -41,25 +42,23 @@ def delete(id):
         db.session.commit()
         return redirect('/')
     except:
-        return 'There was a Problem deleting that task'
-
+        return 'There was a problem deleting that task'
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     task = Todo.query.get_or_404(id)
 
     if request.method == 'POST':
-        task_content = request.form['content']
+        task.content = request.form['content']
 
         try:
             db.session.commit()
             return redirect('/')
         except:
-            return 'There was an issue updateing your task'
-        
+            return 'There was an issue updating your task'
+
     else:
         return render_template('update.html', task=task)
-
 
 
 if __name__ == "__main__":
